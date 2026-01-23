e1a1546e4f4b5a177c8c701f0527bf5d61e6167d
jayneeljariwala <jayneel2001@gmail.com>
2026-01-24 00:03:35 +0530

    Complete 3.7 and add gitignore

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -12,7 +12,7 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureLoggerService();
 
 // Add services to the container.
-
+builder.Services.ConfigureRepositoryManager();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,6 @@
+public interface IRepositoryManager
+{
+    ICompanyRepository Company { get; }
+    IEmployeeRepository Employee { get; }
+    void Save();
+}
