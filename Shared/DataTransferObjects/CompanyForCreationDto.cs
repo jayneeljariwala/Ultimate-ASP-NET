57de0ff7205a3cd4fea458bf1ae4b7c6451caf84
jayneeljariwala <jayneel2001@gmail.com>
2026-02-26 21:16:28 +0530

    Complete 9.4

--- Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -1,1 +1,1 @@
-public record CompanyForCreationDto(string Name, string Address, string Country);
+public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
