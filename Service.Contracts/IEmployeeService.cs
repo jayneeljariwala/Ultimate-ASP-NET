6ce92a0e576bc2fccefd8422540213f7fc971c25
jayneeljariwala <jayneel2001@gmail.com>
2026-03-07 15:17:58 +0530

    Complete 11.1.1

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -6,4 +6,5 @@ public interface IEmployeeService
     EmployeeDTO GetEmployee(Guid companyId, Guid id, bool trackChanges);
     EmployeeDTO CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employeeForCreationDto, bool trackChanges);
     void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges);
+    void UpdateEmployeeForCompany(Guid companyId, Guid id, EmployeeForUpdateDto employeeForUpdateDto, bool companyTrackChanges, bool employeeTrackChanges);
 }
---
+++ Shared/DataTransferObjects/EmployeeForUpdateDto.cs
@@ -0,0 +1,1 @@
+public record EmployeeForUpdateDto(string Name, int Age, string Position);
